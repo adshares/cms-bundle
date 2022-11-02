737e9259d16bb305857f2d258ddda91d43e2a35c
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2022-11-02 13:26:51 +0100

    Add DAO tag

--- src/Entity/ArticleTag.php
+++ src/Entity/ArticleTag.php
@@ -4,17 +4,35 @@ namespace Adshares\CmsBundle\Entity;
 
 enum ArticleTag: string
 {
-    case AdTech = 'AdTech';
-    case Announcement = 'Announcement';
+    case AdTech = 'adtech';
+    case Announcement = 'announcement';
     case AdServer = 'adserver';
     case Application = 'application';
     case Blockchain = 'blockchain';
     case Coin = 'coin';
     case Community = 'community';
+    case DAO = 'dao';
     case Ecosystem = 'ecosystem';
     case Media = 'media';
     case Metaverse = 'metaverse';
     case Protocol = 'protocol';
-    case Technical = 'Technical';
+    case Technical = 'technical';
     case Wallet = 'wallet';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::AdTech => 'Ad Tech',
+            default => $this->name,
+        };
+    }
+
+    public static function options(): array
+    {
+        $options = [];
+        foreach (self::cases() as $case) {
+            $options[$case->label()] = $case->value;
+        }
+        return $options;
+    }
 }
